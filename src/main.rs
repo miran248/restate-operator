1afcbad51bdb5a546cf0c7c26d344317c6a364cd
Miran <miran@248.sh>
2026-03-04 22:14:48 +0100

    fix: attempt to fix the `Error: Address in use (os error 98)`

--- src/main.rs
+++ src/main.rs
@@ -117,7 +117,7 @@ async fn main() -> anyhow::Result<()> {
             .service(health)
             .service(metrics)
     })
-    .bind("0.0.0.0:8080")?
+    // .bind("0.0.0.0:8080")?
     .bind("[::]:8080")?
     .shutdown_timeout(5)
     .run();
